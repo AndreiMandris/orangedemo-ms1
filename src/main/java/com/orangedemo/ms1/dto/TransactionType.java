4477a517339dc07f915a0d96dc925d80f7cd2d68
MANDRIS Andrei <amandris@luxoft.com>
2019-08-02 11:50:29 +0300

    removed reduntant serialization of TransactionType enum

--- src/main/java/com/orangedemo/ms1/dto/TransactionType.java
+++ src/main/java/com/orangedemo/ms1/dto/TransactionType.java
@@ -1,10 +1,5 @@
 package com.orangedemo.ms1.dto;
 
-import java.io.Serializable;
-
-public enum TransactionType implements Serializable {
-
+public enum TransactionType {
     IBAN_TO_IBAN, IBAN_TO_WALLET, WALLET_TO_IBAN, WALLET_TO_WALLET;
-
-    private static final long serialVersionUID = 42L;
 }
